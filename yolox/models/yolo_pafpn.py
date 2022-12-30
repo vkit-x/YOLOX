d8127c496bcb21c0a44b9cf193979c7112341aa0
Hunt Zhan <huntzhan.dev@gmail.com>
2022-12-30 22:57:49 +0800

    comment.

--- yolox/models/yolo_pafpn.py
+++ yolox/models/yolo_pafpn.py
@@ -103,20 +103,20 @@ class YOLOPAFPN(nn.Module):
 
         fpn_out0 = self.lateral_conv0(x0)  # 1024->512/32
         f_out0 = self.upsample(fpn_out0)  # 512/16
-        f_out0 = torch.cat([f_out0, x1], 1)  # 512->1024/16
-        f_out0 = self.C3_p4(f_out0)  # 1024->512/16
+        f_out0 = torch.cat([f_out0, x1], 1)  # 512->1024/16, f_out0'
+        f_out0 = self.C3_p4(f_out0)  # 1024->512/16, f_out0''
 
         fpn_out1 = self.reduce_conv1(f_out0)  # 512->256/16
         f_out1 = self.upsample(fpn_out1)  # 256/8
-        f_out1 = torch.cat([f_out1, x2], 1)  # 256->512/8
+        f_out1 = torch.cat([f_out1, x2], 1)  # 256->512/8, f_out1'
         pan_out2 = self.C3_p3(f_out1)  # 512->256/8
 
         p_out1 = self.bu_conv2(pan_out2)  # 256->256/16
-        p_out1 = torch.cat([p_out1, fpn_out1], 1)  # 256->512/16
+        p_out1 = torch.cat([p_out1, fpn_out1], 1)  # 256->512/16, p_out1'
         pan_out1 = self.C3_n3(p_out1)  # 512->512/16
 
         p_out0 = self.bu_conv1(pan_out1)  # 512->512/32
-        p_out0 = torch.cat([p_out0, fpn_out0], 1)  # 512->1024/32
+        p_out0 = torch.cat([p_out0, fpn_out0], 1)  # 512->1024/32, p_out0'
         pan_out0 = self.C3_n4(p_out0)  # 1024->1024/32
 
         outputs = (pan_out2, pan_out1, pan_out0)
